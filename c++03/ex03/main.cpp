ca4933d63cf688f30110260a5e0623a63a6fd426
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2024-07-18 14:48:55 +0200

    Changed main

--- c++03/ex03/main.cpp
+++ c++03/ex03/main.cpp
@@ -4,24 +4,22 @@
 int	main()
 {
 	DiamondTrap	ConfusedOne;
-	ClapTrap	Sjon(std::string("SJON"));
-	FragTrap	Happy(std::string("Happy"));
-	FragTrap	Clown;
-	FragTrap	Random;
+	// FragTrap	Happy(std::string("Happy"));
+	// FragTrap	Clown;
+	// ScavTrap	Random;
 
-	Clown.setName("Clown");
-	Clown.setAttack(2);
-	Random.attack(Clown.getName());
-	for (int i = 0; i < 2; i++)
-	{
-		Happy.attack(Clown.getName());
-		Clown.takeDamage(Happy.getAttack());
-		Clown.beRepaired(2);
-		Random.highFivesGuys();
-		ConfusedOne.whoAmI();
-		ConfusedOne.attack(Happy.getName());
-		ConfusedOne.takeDamage(1);
-		ConfusedOne.beRepaired(1);
-		ConfusedOne.highFivesGuys();
-	}
+	// Clown.setName("Clown");
+	// Clown.setAttack(2);
+	// Random.attack(Clown.getName());
+	// for (int i = 0; i < 2; i++)
+	// {
+	// 	Happy.attack(Clown.getName());
+	// 	Clown.takeDamage(Happy.getAttack());
+	// 	Clown.beRepaired(2);
+	// 	ConfusedOne.whoAmI();
+	// 	ConfusedOne.attack(Happy.getName());
+	// 	ConfusedOne.takeDamage(1);
+	// 	ConfusedOne.beRepaired(1);
+	// 	ConfusedOne.highFivesGuys();
+	// }
 }
