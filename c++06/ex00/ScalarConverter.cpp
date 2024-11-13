4bd959322c61ece385600ced95b869e1763c5c94
Vincent Van Der Fluit <vvan-der@f0r4s19.codam.nl>
2024-11-13 17:26:58 +0100

    whoops

--- c++06/ex00/ScalarConverter.cpp
+++ c++06/ex00/ScalarConverter.cpp
@@ -9,19 +9,9 @@ enum	types
 	INVALID
 };
 
-static void	printOutput(const std::string (&output)[4])
+static void	checkInfinities(const std::string& input, std::string (&output)[4])
 {
-	std::cout << output[CHAR] << std::endl;
-	std::cout << output[INT] << std::endl;
-	std::cout << output[FLOAT] << std::endl;
-	std::cout << output[DOUBLE] << std::endl;
-}
-
-void	ScalarConverter::convert(const std::string& input)
-{
-	const std::string	infs[8] = {"nan", "nanf", "-inf", "-inff", "+inf", "+inff", "inf", "inff"};
-
-	std::string	output[4] = {"char: ", "int :", "float: ", "double: "};
+	const std::string	infs[8] = {"nan", "nanf", "-inf", "+inf", "-inff", "+inff", "inf", "inff"};
 
 	for (int i = 0; i < 8; i++)
 	{
@@ -34,5 +24,27 @@ void	ScalarConverter::convert(const std::string& input)
 			}
 		}
 	}
+}
+
+static void	charLiteral(const std::string& input, std::string (&output)[4])
+{
+
+}
+
+static void	printOutput(const std::string (&output)[4])
+{
+	std::cout << output[CHAR] << std::endl;
+	std::cout << output[INT] << std::endl;
+	std::cout << output[FLOAT] << std::endl;
+	std::cout << output[DOUBLE] << std::endl;
+}
+
+void	ScalarConverter::convert(const std::string& input)
+{
+	std::string	output[4] = {"char: ", "int :", "float: ", "double: "};
+
+	if (input.size() == 3 && input[0] == '\'' && input[2] == '\'')
+		charLiteral(input, output);
+	// checkInfinities()
 
 }
