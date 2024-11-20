feb9b85ed63176985f2533d328f1617622e54cf1
Soepgroente <117657433+Soepgroente@users.noreply.github.com>
2024-11-20 15:08:22 +0100

    ex00

---
+++ c++07/ex00/Whatever.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+template<typename T>
+void	swap(T& first, T& second)
+{
+	T	tmp;
+
+	tmp = second;
+	second = first;
+	first = tmp;
+}
+
+template<typename T>
+T&	min(T& first, T& second)
+{
+	return ((first < second) ? first : second);
+}
+
+template<typename T>
+T&	max(T& first, T& second)
+{
+	return ((first > second) ? first : second);
+}
